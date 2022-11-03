251b2dcf628635ccc1399fe20ee655f0f74709bf
Akai12138 <108135350+Akai12138@users.noreply.github.com>
2022-11-03 22:22:55 +0000

    Add files via upload

---
+++ counter.sv
@@ -0,0 +1,20 @@
+module counter #(
+    parameter WIDTH = 8
+
+)
+(
+    // interface signals
+    input   logic             clk, //clock
+    input   logic             rst,//reset 
+    input   logic             en,
+    output  logic [WIDTH-1:0] count
+);
+
+always_ff @(posedge clk)
+ begin
+    if(rst ) count <= {WIDTH{1'b0}};
+    else     count <= count + {{WIDTH-1{1'b0}},en};
+    
+end
+    
+endmodule
